e51fcf814536c9f4b9d9ee0173c66a1da438a6a8
Jesse <2302541+Kangaroux@users.noreply.github.com>
2020-08-06 00:41:29 -0400

    Add package doc

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package bleach is a lightweight but powerful library for sanitizing and validating inputs.
+
+Bleach uses two main concepts: checkers and mutators. A checker validates an input and returns an
+error if there is an issue, while a mutator modifies an input and returns its new value.
+*/
+package bleach
