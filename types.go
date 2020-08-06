1cdb31ab91b9566e03fb36674e5159247bbb7488
Jesse <2302541+Kangaroux@users.noreply.github.com>
2020-08-06 02:43:06 -0400

    Allow CheckFunc and MutatorFunc to be used as a wrapper

--- types.go
+++ types.go
@@ -4,6 +4,13 @@ package bleach
 // the CheckFunc should return an error with the reason why.
 type CheckFunc func(interface{}) error
 
+var _ Checker = (CheckFunc)(nil)
+
+// Check calls the CheckFunc.
+func (fn CheckFunc) Check(in interface{}) error {
+	return fn(in)
+}
+
 // Checker is an interface for objects which can be used to check values.
 type Checker interface {
 	Check(interface{}) error
@@ -11,6 +18,13 @@ type Checker interface {
 
 // MutatorFunc is a function which accepts a value, modifies it, and then returns it.
 type MutatorFunc func(interface{}) interface{}
+
+var _ Mutator = (MutatorFunc)(nil)
+
+// Mutate calls the MutatorFunc.
+func (fn MutatorFunc) Mutate(in interface{}) interface{} {
+	return fn(in)
+}
 
 // Mutator is an interface for objects which can mutate a value.
 type Mutator interface {
